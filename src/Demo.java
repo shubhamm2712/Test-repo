4bf6fb0a4615ab9a40d63114d0857d5bd1c17803
shubhamm2712 <shubham.189301129@muj.manipal.edu>
2022-01-18 11:14:47 +0530

    First commit for eclipse

---
+++ src/Demo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Shubham
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println('Hello world');
+	}
+
+}
